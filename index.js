41cb16f6e5c65d66d49067be5aa9d01e0563b725
sundar <nagasundar.selvaraj@axesstechnologymail.in>
2024-05-03 16:42:48 +0530

    action changes

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ require("dotenv").config();
 const router = require("./Router/route.js")
 
 
-const PORT = process.env.PORT
+//const PORT = process.env.PORT
+const PORT = 4000;
 
 app.use(cors());
 app.use(express.json());
@@ -21,7 +22,8 @@ app.get('/', function (req, res) {
 
 const connectToDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI,);
+        //await mongoose.connect(process.env.MONGODB_URI,);
+        await mongoose.connect("mongodb+srv://axessmongo:admin@cluster0.ozjlhdj.mongodb.net/testing?retryWrites=true&w=majority");
         console.log('Connected to MongoDB');
         app.listen(PORT, () => {
             console.log('Server is listening on port ' + PORT);
