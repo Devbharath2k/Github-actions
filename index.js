e4cdb4120c056e8c54dfa85100617118d3f6cfed
sundar <nagasundar.selvaraj@axesstechnologymail.in>
2024-05-03 19:01:53 +0530

    new

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.get('/', function (req, res) {
 
 const connectToDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI,);
-        //await mongoose.connect("mongodb+srv://axessmongo:admin@cluster0.ozjlhdj.mongodb.net/testing?retryWrites=true&w=majority");
+        //await mongoose.connect(process.env.MONGODB_URI,);
+        await mongoose.connect("mongodb+srv://axessmongo:admin@cluster0.ozjlhdj.mongodb.net/testing?retryWrites=true&w=majority");
         console.log('Connected to MongoDB');
         app.listen(PORT, () => {
             console.log('Server is listening on port ' + PORT);
